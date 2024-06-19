f239fb94837fd696daf9152bd8b217ac3cc10dd9
PC <PC@Belefqiih>
2024-06-19 14:05:16 +0100

    a message

--- demo-project/src/main/java/com/test/MyTest.java
+++ demo-project/src/main/java/com/test/MyTest.java
@@ -3,5 +3,9 @@ package com.test;
 public class MyTest {
 	public static void main(String[] args) {
 		System.out.println("salut tout le monde");
+		
+		int i = 3; 
+		int j = 4 ; 
+		System.out.println(i+j);
 	}
 }
